5e31912ee7a0632c790b7adf0e611b234f3de5b3
Mubarik <burkatech2021@gmail.com>
2022-12-01 04:32:06 -0800

    updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,83 +1,53 @@
 #!/usr/bin/python3
 
-from sys import argv
-
-from calculator_1 import add, sub, mul, div
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     
-    """
-
-    Handles basic operations
-
-
-
-    Performs basic operations like addition, substraction,
-
-    multiplication and division between two numbers.
-
-
-
-    The program will execute an operation between two numbers
-
-    selected by the operator sent to the program.
-
-    """
-    
-    l_av = len(argv) - 1
+    import sys
     
 
     
-    if l_av == 3:
+    nargs = len(sys.argv) - 1
+    
+    if nargs != 3:
         
-        operator = argv[2]
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         
-        num_a = int(argv[1])
+        sys.exit(1)
         
-        num_b = int(argv[3])
+
         
-        if operator == '+':
-            
-            res = add(num_a, num_b)
-            
-            print('{:d} + {:d} = {:d}'.format(num_a, num_b, res))
-            
-            exit(0)
-            
-        elif operator == '-':
-            
-            res = sub(num_a, num_b)
-            
-            print('{:d} - {:d} = {:d}'.format(num_a, num_b, res))
-            
-            exit(0)
-            
-        elif operator == '*':
-            
-            res = mul(num_a, num_b)
-            
-            print('{:d} * {:d} = {:d}'.format(num_a, num_b, res))
-            
-            exit(0)
-            
-        elif operator == '/':
-            
-            res = div(num_a, num_b)
-            
-            print('{:d} / {:d} = {:d}'.format(num_a, num_b, res))
-            
-            exit(0)
-            
-        else:
-            
-            print('Unknown operator. Available operators: +, -, * and /')
-            
-            exit(1)
-            
+    op = sys.argv[2]
+    
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        
+        print("Unknown operator. Available operators: +, -, * and /")
+        
+        sys.exit(1)
+        
+
+        
+    from calculator_1 import add, sub, mul, div
+    
+    a = int(sys.argv[1])
+    
+    b = int(sys.argv[3])
+    
+
+    
+    if op == '+':
+        
+        print("{} + {} = {}".format(a, b, add(a, b)))
+        
+    elif op == '-':
+        
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+        
+    elif op == '*':
+        
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+        
     else:
         
-        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
-        
-        exit(1)
+        print("{} / {} = {}".format(a, b, div(a, b)))
