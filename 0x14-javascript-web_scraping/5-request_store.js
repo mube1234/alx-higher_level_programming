a3829813402604990aa4a68619bc0a9f28598aa6
Mubarik <burkatech2021@gmail.com>
2023-04-26 06:17:48 -0700

    updated tasks

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 
-const request = require('request');
+const requests = require('request');
 const fs = require('fs');
 const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    fs.writeFile(filePath, body, 'utf-8');
+const file = process.argv[3];
+requests.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
   }
+  fs.writeFile(file, body, 'utf8', function (error) {
+    if (error) {
+      console.log(error);
+    }
+  });
 });
